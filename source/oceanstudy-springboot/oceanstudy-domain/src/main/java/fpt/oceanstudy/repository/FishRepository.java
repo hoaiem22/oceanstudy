8d7a53eb573617c66b07cf505ce04e5595ddfee8
TOANNBSE62396\bachtoan <nguyenbachtoan120997@gmail.com>
2018-10-31 17:38:01 +0700

    Update source/oceanstudy-springboot/oceanstudy-domain/src/main/java/fpt/oceanstudy/repository/FishRepository.java

--- source/oceanstudy-springboot/oceanstudy-domain/src/main/java/fpt/oceanstudy/repository/FishRepository.java
+++ source/oceanstudy-springboot/oceanstudy-domain/src/main/java/fpt/oceanstudy/repository/FishRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.repository.CrudRepository;
 import fpt.oceanstudy.entity.OsFish;
 
 public interface FishRepository extends CrudRepository<OsFish, Integer>{
-
     @Query(nativeQuery = true, value = "SELECT id, name, weight, length, height, deep, age, img, video FROM oceanstudy.os_fish\r\n" + 
             "order by deep asc")
     List<OsFish> sortByDeep();
