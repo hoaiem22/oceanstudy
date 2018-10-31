6865928d5227c63a54d1642ed7172c87dd3ac43e
TOANNBSE62396\bachtoan <nguyenbachtoan120997@gmail.com>
2018-10-31 17:36:17 +0700

    change local
    abc

---
+++ source/oceanstudy-android-thanh/app/src/main/java/hci201/se1171/oceanstudy/BackgroundMusicService.java
@@ -0,0 +1,54 @@
+package hci201.se1171.oceanstudy;
+
+import android.annotation.SuppressLint;
+import android.app.Service;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+public class BackgroundMusicService extends Service{
+    MediaPlayer mp_object;
+    public BackgroundMusicService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+//        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        mp_object = MediaPlayer.create(getApplicationContext(), R.raw.funny_background);
+        mp_object.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        startService(new Intent(this, BackgroundMusicService.class));
+    }
+
+    @SuppressLint("WrongConstant")
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+
+        mp_object.setLooping(true);
+        mp_object.start();
+
+        return 0;
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+    }
+
+
+
+
+
+
+
+}
