493705267735ab41378826be2267be25c5a73d7d
EmVH <hoaiem.heli22@gmail.com>
2018-10-31 18:26:32 +0700

    Static

---
+++ source/fishstudy-android/fish-study-static/app/src/main/java/hci201/se1171/oceanstudy/QuizData.java
@@ -0,0 +1,20 @@
+package hci201.se1171.oceanstudy;
+
+public class QuizData {
+
+    Integer[] fishes = {
+            R.drawable.dolphin,
+            R.drawable.shark,
+            R.drawable.seahorse,
+            R.drawable.octopus,
+            R.drawable.whale
+    };
+
+    String[] answers = {
+            "cá heo",
+            "cá mập",
+            "cá ngựa",
+            "bạch tuộc",
+            "cá voi"
+    };
+}
